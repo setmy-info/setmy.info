f6942a77cb32d301619b03b7d3f3e1ff3679c7b3
Imre Tabur <imre.tabur@eesti.ee>
2018-09-26 08:08:32 +0300

    NameEntity enhancements

--- java-models/src/main/java/info/setmy/models/NamedEntity.java
+++ java-models/src/main/java/info/setmy/models/NamedEntity.java
@@ -3,9 +3,8 @@ package info.setmy.models;
 /**
  *
  * @author <a href="mailto:imre.tabur@eesti.ee">Imre Tabur</a>
- * @param <T> entity ID type.
  */
-public abstract class NamedEntity<T> extends Entity<T> {
+public abstract class NamedEntity extends Entity {
 
     private String name;
 
