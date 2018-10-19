e7902ddd48efaf9f739097c64c94517767559a3a
Imre Tabur <imre.tabur@eesti.ee>
2018-10-19 08:27:46 +0300

    PoC.

---
+++ java-accounting/src/test/java/info/setmy/accounting/poc/BICCode.java
@@ -0,0 +1,14 @@
+package info.setmy.accounting.poc;
+
+public class BICCode {
+
+    // AAAABBCCDDD form
+    private String bankCode;
+
+    // ISO-3166-1 alpha-2 country code
+    private String countryCode;
+
+    private String locationCode;
+
+    private String branchCode;
+}
---
+++ java-accounting/src/test/java/info/setmy/accounting/poc/Misc.java
@@ -0,0 +1,6 @@
+package info.setmy.accounting.poc;
+
+public class Misc {
+
+    public BICCode bicCode;
+}
