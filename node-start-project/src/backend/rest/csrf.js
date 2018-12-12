e6c33d67f51592f7a468e0d44a7a28e96e817530
Imre Tabur <imre.tabur@eesti.ee>
2018-12-12 08:27:05 +0200

    CSRF example parts removed.

--- node-start-project/src/backend/rest/csrf.js
+++ node-start-project/src/backend/rest/csrf.js
@@ -3,20 +3,7 @@ var router = express.Router();
 
 router.get('/picture', function (req, res) {
     var security = {
-
     };
-    for (var header in req.headers) {
-        console.log(JSON.stringify(header));
-        if (header.toUpperCase() === "REFERER") {
-            if (req.headers[header].indexOf("web.site") === -1) {
-                security.CSRF_ATTACK = true;
-            }
-        } else if (header.toUpperCase() === "ORIGIN") {
-            if (req.headers[header].indexOf("web.site") === -1) {
-                security.CSRF_ATTACK = true;
-            }
-        }
-    }
     res.send({headers: req.headers, security: security});
 });
 
