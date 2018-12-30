ca795006d5ff8b06602725e58ba9c5dcd4c06583
Imre Tabur <imre.tabur@eesti.ee>
2018-12-30 22:21:12 +0200

    Portainer executable microservices set

--- micronaut-start-project/src/main/java/info/setmy/controllers/HelloController.java
+++ micronaut-start-project/src/main/java/info/setmy/controllers/HelloController.java
@@ -5,7 +5,7 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
 
-@Controller("/hello")
+@Controller("/rest/hello")
 public class HelloController {
 
     @Get("/")
