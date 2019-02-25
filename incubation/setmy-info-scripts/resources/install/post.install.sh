eab33de5c105984b8d84dba3c2b95b3f7782af94
Imre Tabur <imre.tabur@eesti.ee>
2019-02-25 20:26:40 +0200

    Some base scripts enhancements

--- incubation/setmy-info-scripts/resources/install/post.install.sh
+++ incubation/setmy-info-scripts/resources/install/post.install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Executed by installer at install step end.
-ln -f -s /opt/setmy.info/etc/profile.d/example.sh /etc/profile.d/example.sh
+ln -f -s /opt/setmy.info/etc/profile.d/setmy-info.sh /etc/profile.d/setmy-info.sh
+ln -f -s /opt/setmy.info/bin/smi-binary /opt/setmy.info/bin/smi-test
 
 exit ${?}
--- incubation/setmy-info-scripts/resources/install/post.uninstall.sh
+++ incubation/setmy-info-scripts/resources/install/post.uninstall.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # De-install script, executed at uninstall end.
-rm -f /etc/profile.d/example.sh
+rm -f /etc/profile.d/setmy-info.sh
+rm -f /opt/setmy.info/bin/smi-test
 
 exit ${?}
