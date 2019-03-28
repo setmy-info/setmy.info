98c8ae6ec51df5cea109e27c5903e5464fe2ecf1
Imre Tabur <imre.tabur@eesti.ee>
2019-03-28 22:58:46 +0200

    Other Go example

---
+++ incubation/go-start-project/src/example/main.go
@@ -0,0 +1,45 @@
+package example
+
+import "fmt"
+import "os"
+
+type Person struct {
+	FirstName string
+	LastName string
+}
+
+func main() {
+	fmt.Println("Hello, 世界")
+	os.Exit(0)
+}
+
+func foo(name string) int {
+	var expected = "Hello World!"
+	if name == expected {
+		return 1
+	}
+	return 0
+}
+
+func stringing() int {
+	var str = "Hello World!"
+	got := foo(str) // nil not allowed
+	return got
+}
+
+func structing() *Person {
+	person := new(Person)
+	person.FirstName = "Peeter-Meeter"
+	person.LastName = "Sentimeeter"
+	fmt.Println(Person{"Bob", "Meter 1"})
+	fmt.Println(Person{FirstName: "Alice", LastName: "Meter 2"})
+	fmt.Println(Person{FirstName: "Fred 3"})
+	fmt.Println(&Person{FirstName: "Ann", LastName: "Meter 4"})
+	s := Person{FirstName: "Sean", LastName: "Meter 5"}
+    fmt.Println(s.FirstName)
+    sp := &s
+    fmt.Println(sp.LastName)
+	sp.LastName = "Meter 6"
+    fmt.Println(sp.LastName)
+	return person
+}
