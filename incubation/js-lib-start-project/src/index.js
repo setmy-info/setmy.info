9b05a335e3d14991eec9a706bc16a28af79ffb83
Imre Tabur <imre.tabur@eesti.ee>
2019-04-26 08:29:11 +0300

    Enhancements

--- incubation/js-lib-start-project/src/index.js
+++ incubation/js-lib-start-project/src/index.js
@@ -4,6 +4,7 @@ import srvdjs from 'servedjs';
 
 function component() {
   let element = document.createElement('div');
+  var geoWatcher = jsdi.get().$geo.newWatcher(function (position) {console.log("Position: ", position);});
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
