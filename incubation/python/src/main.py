f3b32e31990cacc37a245de6966e5b018e5ecae2
Imre Tabur <imre.tabur@eesti.ee>
2019-05-22 07:18:22 +0300

    Enhancements for logging

--- incubation/python/src/main.py
+++ incubation/python/src/main.py
@@ -10,9 +10,11 @@ from setmy.info.rest.index import index
 app = Flask(__name__)
 conf.load()
 
+
 @app.route(conf.path("/"))
 def idx():
     return index()
+
 
 if __name__ == '__main__':
     app.run()
--- incubation/python/src/setmy/info/config.py
+++ incubation/python/src/setmy/info/config.py
@@ -5,15 +5,34 @@ Created on 20. mai 2019
 '''
 
 import yaml
+import logging
+
+CONFIG_FILE_NAME = "./config/application.yml"
+FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
 
 global conf
 
-CONFIG_FILE_NAME = "./config/application.yml"
+class Config:
 
+    def __init__(self):
+        self.yamlLoader = YamlLoader()
+
+    def load(self):
+        self.yamlLoader.load()
+
+    def path(self, pathName):
+        return self.yamlLoader.loaded['rest']['root'] + pathName
+    
 
 class YamlLoader:
 
     def load(self):
+        #format=FORMAT, 
+        logging.basicConfig(level=logging.DEBUG)
+        logging.warning('Watch out!')
+        logging.info('I told you so')
+        logging.debug('This message should go to the log file')
+        logging.info('Name: ' + __name__ + ' : ' + str((__name__ == '__main__') == True))
         with open(CONFIG_FILE_NAME, 'r') as stream:
             try:
                 self.loaded = yaml.safe_load(stream)
@@ -24,9 +43,6 @@ class YamlLoader:
                 print(exc)
         return self.loaded
 
-    def path(self, pathName):
-        return self.loaded['rest']['root'] + pathName
 
-
-conf = YamlLoader()
+conf = Config()
 
