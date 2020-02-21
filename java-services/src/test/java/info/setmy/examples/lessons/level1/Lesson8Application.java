a1a88d54a579cb2b1291df78a81086f6d7af70c7
Imre Tabur <imre.tabur@eesti.ee>
2020-02-21 08:25:37 +0200

    Moved files and app added

---
+++ java-services/src/test/java/info/setmy/examples/lessons/level1/Lesson8Application.java
@@ -0,0 +1,12 @@
+package info.setmy.examples.lessons.level1;
+
+/**
+ *
+ * @author <a href="mailto:imre.tabur@eesti.ee">Imre Tabur</a>
+ */
+public class Lesson8Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
