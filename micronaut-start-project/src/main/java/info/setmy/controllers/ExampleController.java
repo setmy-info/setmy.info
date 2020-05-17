84ebda2f4b596ebcef0e30eceb2d203ef4672327
Imre Tabur <imre.tabur@eesti.ee>
2020-05-17 14:56:04 +0300

    Micrnaout updates

--- micronaut-start-project/src/main/java/info/setmy/controllers/ExampleController.java
+++ micronaut-start-project/src/main/java/info/setmy/controllers/ExampleController.java
@@ -3,8 +3,9 @@ package info.setmy.controllers;
 import static info.setmy.constants.CacheConstants.CACHE_NAME;
 import info.setmy.models.ExampleModel;
 import info.setmy.properties.ExampleProperties;
-import io.micronaut.cache.annotation.Cacheable;
+//import io.micronaut.cache.annotation.Cacheable;
 import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Consumes;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
@@ -38,7 +39,8 @@ public class ExampleController {
 
     @Get("/example")
     @Produces(MediaType.APPLICATION_JSON)
-    @Cacheable(CACHE_NAME)
+    @Consumes(MediaType.APPLICATION_JSON)
+    //@Cacheable(CACHE_NAME)
     public ExampleModel example() {
         log.info("Micronaut controller called!!");
         final ExampleModel exampleModel = new ExampleModel();
--- micronaut-start-project/src/test/java/info/setmy/ExampleTest.java
+++ micronaut-start-project/src/test/java/info/setmy/ExampleTest.java
@@ -1,6 +1,6 @@
 package info.setmy;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
