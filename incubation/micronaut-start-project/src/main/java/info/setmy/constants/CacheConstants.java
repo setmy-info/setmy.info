4efa152faf05f3051be59f2a8c2c7361b7871e58
Imre Tabur <imre.tabur@eesti.ee>
2021-06-19 19:06:43 +0300

    Micronaut removed

--- incubation/micronaut-start-project/src/main/java/info/setmy/constants/CacheConstants.java
+++
@@ -1,9 +0,0 @@
-package info.setmy.constants;
-
-public final class CacheConstants {
-
-    public final static String CACHE_NAME = "micronaut-start-project";
-
-    private CacheConstants() {
-    }
-}
