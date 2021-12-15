9ee4399fedc66e1118a02afeed5ddb33893a7056
Imre Tabur <imre.tabur@eesti.ee>
2021-12-15 12:45:34 +0200

    Loger fix for ShotDown component

--- springboot-start-project/src/main/java/info/setmy/microservice/components/ShotDownComponent.java
+++ springboot-start-project/src/main/java/info/setmy/microservice/components/ShotDownComponent.java
@@ -5,15 +5,11 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PreDestroy;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Log4j2
 @Component
 @RequiredArgsConstructor
 public class ShotDownComponent {
-
-    final Logger log = LogManager.getLogger(getClass());
 
     @PreDestroy
     public void preDestroy() throws Exception {
