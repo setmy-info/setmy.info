e885b0788c21106906244eb377b33a1afb110eee
Imre Tabur <imre.tabur@eesti.ee>
2021-12-17 12:44:37 +0200

    Test for JUnit 5 vl 2

--- java-reports/src/test/java/info/setmy/reports/JasperReportsServiceIT.java
+++ java-reports/src/test/java/info/setmy/reports/JasperReportsServiceIT.java
@@ -3,10 +3,14 @@ package info.setmy.reports;
 import info.setmy.models.storage.DirectoryStructurePattern;
 import info.setmy.models.storage.Storage;
 import info.setmy.models.storage.StorageFile;
+import java.io.File;
 import java.util.Arrays;
 import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 /**
  *
@@ -45,8 +49,11 @@ public class JasperReportsServiceIT {
     }
 
     @Test
+    @EnabledOnOs({OS.LINUX})
     public void testExport() {
         final Optional<StorageFile> file = storage.createStorageFile(new DirectoryStructurePattern().setDefault());
-        jasperReportsService.export(model, file.get().getChild());
+        final File child = file.get().getChild();
+        assertThat(child).isNotNull();
+        jasperReportsService.export(model, child);
     }
 }
