4bd30822eb07de46c2d0c6f38db4e7549eae9ab2
Imre Tabur <imre.tabur@eesti.ee>
2022-05-12 21:38:26 +0300

    More lambdas

--- incubation/python-start-project/project/lessons/lesson2/Lesson2.py
+++ incubation/python-start-project/project/lessons/lesson2/Lesson2.py
@@ -16,5 +16,10 @@ if __name__ == "__main__":
     print(lamb(3))
     def l1(x): return 2 * x
     def l2(x, y): return x + y
+    def l3(x, y): return x if x > y else y
     print(l1(4))
     print(l2(4, 2))
+    print(l3(2, 7))
+    n = [4, 3, 2, 1]
+    print(n)
+    print(list(map(lambda x: x**2, n)))
