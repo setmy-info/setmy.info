404d0c0c15b611ab415cea1e5d56c4088462150f
Imre Tabur <imre.tabur@eesti.ee>
2022-07-01 17:23:47 +0300

    Cucumber testing changes

--- java-stealer/src/test/java/info/setmy/stealer/CukesIT.java
+++ java-stealer/src/test/java/info/setmy/stealer/CukesIT.java
@@ -1,17 +1,11 @@
 package info.setmy.stealer;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Cucumber;
 
 /**
  *
  * @author <a href="mailto:imre.tabur@eesti.ee">Imre Tabur</a>
  */
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {"pretty", "summary", "html:target/cucumber/examples/cucumber.html", "json:target/cucumber/examples/cucumber.json"},
-        features = {"src/test/cucumber"}
-)
+@Cucumber
 public class CukesIT {
 }
