40da4cdce591d49514854fc9ab71405f7d09e04f
Imre Tabur <imre.tabur@eesti.ee>
2022-09-28 22:06:41 +0300

    Logging works

--- incubation/clojure/src/test/groovy/info/setmy/ClojureCommandTest.groovy
+++ incubation/clojure/src/test/groovy/info/setmy/ClojureCommandTest.groovy
@@ -3,9 +3,6 @@ package info.setmy
 import io.micronaut.configuration.picocli.PicocliRunner
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.env.Environment
-
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
 import org.junit.jupiter.api.Test
 
 import static org.junit.jupiter.api.Assertions.assertTrue
