6b843d5ed8be5d3f6a678d61ab78236963c24110
Imre Tabur <imre.tabur@eesti.ee>
2022-11-26 12:48:05 +0200

    Clojure code moved from incubation to submpodules

--- incubation/clojure/src/test/clojure/info/setmy/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns info.setmy.core-test
-    (:require [clojure.test :refer :all]
-              [info.setmy.core :refer :all]))
-
-(deftest a-test
-    (testing "FIXME, I fail."
-             (is (= 1 1))))
