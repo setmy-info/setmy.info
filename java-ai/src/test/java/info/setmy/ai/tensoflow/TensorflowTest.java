4d8cecc8d79ca9664f60cb1cb89b888931a38092
Imre Tabur <imre.tabur@eesti.ee>
2023-02-13 22:08:46 +0200

    Tensor example

--- java-ai/src/test/java/info/setmy/ai/tensoflow/TensorflowTest.java
+++ java-ai/src/test/java/info/setmy/ai/tensoflow/TensorflowTest.java
@@ -4,15 +4,21 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.tensorflow.ConcreteFunction;
+import org.tensorflow.Graph;
+import org.tensorflow.Operation;
+import org.tensorflow.Session;
 import org.tensorflow.Signature;
 import org.tensorflow.Tensor;
 import org.tensorflow.TensorFlow;
+import org.tensorflow.ndarray.impl.sequence.SingleElementSequence;
 import org.tensorflow.op.Ops;
 import org.tensorflow.op.core.Placeholder;
 import org.tensorflow.op.math.Add;
+import org.tensorflow.types.TFloat64;
 import org.tensorflow.types.TInt32;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.tensorflow.proto.framework.DataType.DT_DOUBLE;
 
 public class TensorflowTest {
 
@@ -27,6 +33,58 @@ public class TensorflowTest {
         final int result = ((TInt32) resultTensor).getInt();
         log.info("{} doubled is {}", number.getInt(), result);
         assertThat(result).isEqualTo(20);
+    }
+
+    @Test
+    public void test2() {
+        // f(x, y) = z = a*x + b*y
+        final Graph graph = new Graph();
+
+        final TFloat64 numberA = TFloat64.scalarOf(3.0);
+        final Operation a = graph.opBuilder("Const", "a")
+            .setAttr("dtype", numberA.dataType())
+            .setAttr("value", numberA)
+            .build();
+
+        final TFloat64 numberB = TFloat64.scalarOf(2.0);
+        final Operation b = graph.opBuilder("Const", "b")
+            .setAttr("dtype", numberB.dataType())
+            .setAttr("value", numberB)
+            .build();
+
+        final Operation x = graph.opBuilder("Placeholder", "x")
+            .setAttr("dtype", DT_DOUBLE)
+            .build();
+
+        final Operation y = graph.opBuilder("Placeholder", "y")
+            .setAttr("dtype", DT_DOUBLE)
+            .build();
+
+        final Operation ax = graph.opBuilder("Mul", "ax")
+            .addInput(a.output(0))
+            .addInput(x.output(0))
+            .build();
+        final Operation by = graph.opBuilder("Mul", "by")
+            .addInput(b.output(0))
+            .addInput(y.output(0))
+            .build();
+        final Operation z = graph.opBuilder("Add", "z")
+            .addInput(ax.output(0))
+            .addInput(by.output(0))
+            .build();
+
+        final Tensor tensorX = TFloat64.scalarOf(3.0);
+        final Tensor tensorY = TFloat64.scalarOf(3.0);
+        final Session sess = new Session(graph);
+        final Tensor resultTensor = sess.runner()
+            .fetch("z")
+            .feed("x", tensorX)
+            .feed("y", tensorY)
+            .run()
+            .<TFloat64>get(0);
+        //.expect(Double.class);
+        final TFloat64 result = (TFloat64) resultTensor;
+        ///assertThat(((SingleElementSequence) result.scalars()).toString()).isEqualTo("xxx");
     }
 
     private Signature functionSignature(final Ops ops) {
