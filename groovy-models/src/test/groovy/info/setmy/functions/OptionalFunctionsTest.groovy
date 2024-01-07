412f66f958aba0ed458db1fc4ad7477c7a06731c
Imre Tabur <imre.tabur@mail.ee>
2024-01-07 17:02:51 +0200

    Groovy example

---
+++ groovy-models/src/test/groovy/info/setmy/functions/OptionalFunctionsTest.groovy
@@ -0,0 +1,29 @@
+package info.setmy.functions
+
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import static info.setmy.functions.OptionalFunctions.blankAsNull
+import static info.setmy.functions.OptionalFunctions.blankToEmpty
+import static java.util.Optional.empty
+
+/**
+ *
+ * @author <a href="mailto:imre.tabur@eesti.ee">Imre Tabur</a>
+ */
+class OptionalFunctionsTest {
+
+    @BeforeEach
+    void before() {
+    }
+
+    @Test
+    void blankAsNull_and_blankToEmpty() {
+        final Optional<String> optionalString = empty();
+        def result = optionalString.map blankAsNull
+        def result2 = blankToEmpty optionalString
+        assert result.isPresent() == false
+        assert result2.isPresent() == false
+    }
+}
