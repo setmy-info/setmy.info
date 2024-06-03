a17066d7ff807cde767e2d59eaa1959ed5359048
Imre Tabur <imre.tabur@mail.ee>
2024-06-03 22:06:52 +0300

    EHcache preparations

---
+++ incubation/springboot-start-project/src/main/java/info/setmy/microservice/bean/EhcacheConfig.java
@@ -0,0 +1,10 @@
+package info.setmy.microservice.bean;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//@EnableCaching
+public class EhcacheConfig {
+
+}
