8f88111fdc810c32c004a4f6285a3b555cca80b9
Imre Tabur <imre.tabur@mail.ee>
2024-07-03 18:02:04 +0300

    XSD simplified for TSD, to hold similarities as are produced. Example XSD added.

--- java-accounting/src/main/java/info/setmy/accouning/tsd/model/TsdForm.java
+++ java-accounting/src/main/java/info/setmy/accouning/tsd/model/TsdForm.java
@@ -1,9 +1,17 @@
 package info.setmy.accouning.tsd.model;
 
 import info.setmy.tsd.models.TsdVorm;
+import info.setmy.tsd.models.VormType;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "tsd_vorm", namespace = "https://www.emta.ee/tsd_2023")
+@XmlRootElement(name = "tsd_vorm")
 public class TsdForm extends TsdVorm {
 
+    public TsdForm() {
+        this(VormType.TSD);
+    }
+
+    public TsdForm(final VormType vormType) {
+        this.setVorm(vormType);
+    }
 }
