746c721e6f2253240b41c2f17348ec7ee0f6e0b5
Imre Tabur <imre.tabur@mail.ee>
2024-10-14 21:16:03 +0300

    Clojure calling Vol 2. Workign without compiling.

--- springboot-start-project/src/main/groovy/info/setmy/microservice/clojure/service/ClojureService.groovy
+++ springboot-start-project/src/main/groovy/info/setmy/microservice/clojure/service/ClojureService.groovy
@@ -1,7 +1,7 @@
 package info.setmy.microservice.clojure.service
 
-
-import clojure.lang.RT
+import clojure.java.api.Clojure
+import clojure.lang.IFn
 import groovy.util.logging.Slf4j
 import org.springframework.stereotype.Service
 
@@ -12,13 +12,13 @@ class ClojureService {
     void exec(final ClojureExec clojureExec) {
         log.info("Execute clojure")
         try {
+            /*
             RT.loadResourceScript(clojureExec.scriptName);
             RT.var(clojureExec.ns, clojureExec.mainFunctionName).invoke(clojureExec.args);
-            //java.lang.IllegalStateException: Attempting to call unbound fn: #'info.setmy.clojure.core/-main
-            /*
+            */
+            Clojure.var("clojure.core", "require").invoke(Clojure.read(clojureExec.ns));
             IFn fun = Clojure.var(clojureExec.ns, clojureExec.mainFunctionName);
             fun.invoke(clojureExec.args);
-            */
         } catch (Exception e) {
             e.printStackTrace();
         }
