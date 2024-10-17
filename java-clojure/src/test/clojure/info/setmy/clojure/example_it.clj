b85715f16e581c718af5ef3cd61ab6c4291164d0
Imre Tabur <imre.tabur@mail.ee>
2024-10-17 07:44:28 +0300

    CLojure module building and testing vol 3

--- java-clojure/src/test/clojure/info/setmy/clojure/example_it.clj
+++ java-clojure/src/test/clojure/info/setmy/clojure/example_it.clj
@@ -5,8 +5,8 @@
 
 (def add +)
 
-(deftest integration-test-add
-    "Unit Test documentation"
+(deftest unit-test-add
+    "Integration Test documentation"
   (testing "Greet test"
     (is (= 5 (add 3 2)))
     (is (= "Hello, Me" (greet "Me")))))
--- java-clojure/src/test/clojure/info/setmy/clojure/example_test.clj
+++ java-clojure/src/test/clojure/info/setmy/clojure/example_test.clj
@@ -5,8 +5,8 @@
 
 (def add +)
 
-(deftest unit-test-add
-    "Integration Test documentation"
+(deftest integration-test-add
+    "Unit Test documentation"
   (testing "Greet test"
     (is (= 5 (add 3 2)))
     (is (= "Hello, Me" (greet "Me")))))
