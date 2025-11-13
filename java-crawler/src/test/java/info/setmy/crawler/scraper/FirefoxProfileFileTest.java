4fac3f524d7556296bd9a466045f3f79d4193002
Imre Tabur <imre.tabur@mail.ee>
2025-11-13 03:25:09 +0200

    Disabled test on CI

--- java-crawler/src/test/java/info/setmy/crawler/scraper/FirefoxProfileFileTest.java
+++ java-crawler/src/test/java/info/setmy/crawler/scraper/FirefoxProfileFileTest.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
 class FirefoxProfileFileTest {
 
     FirefoxProfileFile firefoxProfileFile;
 
     @Test
-    @Disabled
+    @DisabledIfEnvironmentVariable(named = "GITHUB_ACTIONS", matches = "true")
     void test() {
         final String profileName = "selenium-test";
         firefoxProfileFile = new FirefoxProfileFile(profileName);
