04255dce0db96ef44fb9b271d5f47b294870f57c
Imre Tabur <imre.tabur@sfey.com>
2025-11-18 15:42:33 +0200

    Camel example

--- springboot-start-project/src/test/java/info/setmy/microservice/it/camel/CamelIT.java
+++ springboot-start-project/src/test/java/info/setmy/microservice/it/camel/CamelIT.java
@@ -1,8 +1,10 @@
 package info.setmy.microservice.it.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.spi.ThreadPoolProfile;
 import org.junit.jupiter.api.Test;
 
 class CamelIT {
@@ -12,14 +14,24 @@ class CamelIT {
     @Test
     void test() throws Exception {
 
+        ThreadPoolProfile threadPoolProfile = new ThreadPoolProfile();
+        threadPoolProfile.setId("myPool");
+        threadPoolProfile.setPoolSize(5);
+        threadPoolProfile.setMaxPoolSize(5);
+        threadPoolProfile.setMaxQueueSize(0);
+
+        context.getExecutorServiceManager().registerThreadPoolProfile(threadPoolProfile);
+
         context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() {
                 from("direct:start")
                     .routeId("mainRoute")
+                    .threads().executorService("myPool")
                     .log("Entering step 1")
                     .process(exchange -> {
                         String body = exchange.getIn().getBody(String.class);
+                        //Thread.sleep(300);
                         exchange.getIn().setBody(body + " -> Step 1");
                     })
                     .log("Entering step 2")
@@ -36,7 +48,11 @@ class CamelIT {
             }
         });
         context.start();
-        context.createProducerTemplate().sendBody("direct:start", "Algus");
+        ProducerTemplate template = context.createProducerTemplate();
+        for (int i = 0; i < 10; i++) {
+            template.asyncSendBody("direct:start", "Algus " + i);
+        }
+        //context.createProducerTemplate().sendBody("direct:start", "Algus");
         Thread.sleep(2000);
         context.stop();
     }
