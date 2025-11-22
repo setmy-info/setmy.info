690510e35e3e8af9f2d11c5eeea3486ac79f3925
Imre Tabur <imre.tabur@mail.ee>
2025-11-22 12:21:42 +0200

    SCV, Hibernate, iquibase

---
+++ java-crawler/src/main/java/info/setmy/crawler/camel/services/CSVService.java
@@ -0,0 +1,51 @@
+package info.setmy.crawler.camel.services;
+
+import info.setmy.crawler.camel.CamelBean;
+import lombok.extern.log4j.Log4j2;
+import org.apache.camel.Exchange;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+
+@Log4j2
+public class CSVService implements CamelBean {
+
+    private final CSVFormat format = CSVFormat.DEFAULT
+        .withDelimiter(';')
+        .withQuote('"')
+        .withHeader("name", "url")
+        .withIgnoreSurroundingSpaces()
+        .withTrim();
+
+    @Override
+    public String getName() {
+        return "csvService";
+    }
+
+    @Override
+    public void doRun(final Exchange exchange) {
+        final String cswRow = exchange.getIn().getBody(String.class);
+        try {
+            final CSVParser parser = CSVParser.parse(cswRow, format);
+            final CSVRecord record = parser.getRecords().get(0);
+            final info.setmy.crawler.entities.CSVRecord csvRecord = map(record);
+            exchange.getIn().setBody(csvRecord);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private info.setmy.crawler.entities.CSVRecord map(final CSVRecord record) {
+        try {
+            return info.setmy.crawler.entities.CSVRecord.builder()
+                .name(record.get(0))
+                .url(record.get(1))
+                .build();
+        } catch (java.lang.ArrayIndexOutOfBoundsException exception) {
+            log.error("CSV row have error {}", record, exception);
+            throw new RuntimeException(exception);
+        }
+    }
+}
--- java-crawler/src/test/java/info/setmy/crawler/camel/CamelIT.java
+++ java-crawler/src/test/java/info/setmy/crawler/camel/CamelIT.java
@@ -1,10 +1,17 @@
 package info.setmy.crawler.camel;
 
+import info.setmy.crawler.camel.services.CSVService;
 import info.setmy.crawler.dal.DataSourceConfig;
+import info.setmy.crawler.dal.DataSourceFactory;
+import info.setmy.crawler.dal.HibernateComponent;
+import info.setmy.crawler.dal.LiquibaseComponent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.sql.DataSource;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 class CamelIT {
 
@@ -14,16 +21,22 @@ class CamelIT {
     CamelConfig camelConfig;
     Camel camel;
     ExampleService exampleService;
+    CSVService csvService;
     String executorPoolId;
     CamelThreadsConfig camelThreadsConfig;
     String routeId;
     ScraperRouteBuilderConfig scraperRouteBuilderConfig;
     ScraperRouteBuilder routeBuilder;
     DataSourceConfig dataSourceConfig;
+    DataSource dataSource;
+    LiquibaseComponent liquibaseComponent;
+    Map<String, Object> hibernateProperties;
+    HibernateComponent hibernateComponent;
     String baseDirName;
     File baseDir;
     HomeDirectory homeDirectory;
     WorkingDirectory workingDirectory;
+
 
     @BeforeEach
     void setUp() {
@@ -39,6 +52,7 @@ class CamelIT {
             .build();
         camel = new Camel(camelConfig);
         exampleService = new ExampleService();
+        csvService = new CSVService();
         executorPoolId = "testPool";
         camelThreadsConfig = CamelThreadsConfig.builder()
             .poolId("testPool")
@@ -54,18 +68,42 @@ class CamelIT {
             .executorPoolId(executorPoolId)
             .build();
         routeBuilder = new ScraperRouteBuilder(scraperRouteBuilderConfig);
-        routeBuilder.addCamelBean(exampleService);
         dataSourceConfig = DataSourceConfig.builder()
-            .jdbcUrl("jdbc:h2:./target/test-db;AUTO_SERVER=TRUE")
+            .jdbcUrl("jdbc:h2:" + workingDirectory.getOutput().getAbsolutePath() + "/test-db")
             .userName("sa")
             .password("")
             .maximumPoolSize(5)
             .build();
+        dataSource = DataSourceFactory.getInstance().newDataSource(dataSourceConfig);
+        liquibaseComponent = new LiquibaseComponent(dataSource, "db/changelog/db.changelog-master.xml")
+            .migrate();
+        hibernateProperties = new HashMap<>();
+        hibernateProperties.put("hibernate.connection.datasource", dataSource);
+        hibernateProperties.put("hibernate.hikari.dataSource", dataSource);
+        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        hibernateProperties.put("hibernate.hbm2ddl.auto", "validate"); // validate, update, create, create-drop
+        hibernateProperties.put("hibernate.show_sql", "true");
+        hibernateProperties.put("hibernate.format_sql", "true");
+        hibernateProperties.put("hibernate.use_sql_comments", "true");
+        hibernateProperties.put("hibernate.jdbc.batch_size", "20");
+        hibernateProperties.put("hibernate.order_inserts", "true");
+        hibernateProperties.put("hibernate.order_updates", "true");
+        hibernateProperties.put("hibernate.batch_fetch_style", "DYNAMIC");
+        hibernateProperties.put("hibernate.hikari.minimumIdle", "2");
+        hibernateProperties.put("hibernate.hikari.maximumPoolSize", "10");
+        hibernateProperties.put("hibernate.hikari.idleTimeout", "30000");
+        hibernateProperties.put("hibernate.hikari.connectionTimeout", "20000");
+        hibernateProperties.put("hibernate.hikari.maxLifetime", "120000");
+        hibernateProperties.put("jakarta.persistence.nonJtaDataSource", dataSource);
+
+        hibernateComponent = new HibernateComponent(hibernateProperties, dataSource)
+            .init();
     }
 
     @Test
     void camelTest() {
         camel.bind(exampleService);
+        camel.bind(csvService);
         camel.registerThreadPool(camelThreadsConfig);
         camel.add(routeBuilder);
         camel.run();
