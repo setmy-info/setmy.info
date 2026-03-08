acecf4d3bfde9f1ceeb4850ab75cd38480d161e8
Imre Tabur <imre.tabur@mail.ee>
2026-03-08 18:55:16 +0200

    Spring boot changes.

--- springboot-start-project/springboot-start-app/src/test/java/info/setmy/microservice/it/graphql/ExampleGraphQLIT.java
+++ springboot-start-project/springboot-start-app/src/test/java/info/setmy/microservice/it/graphql/ExampleGraphQLIT.java
@@ -1,30 +1,45 @@
 package info.setmy.microservice.it.graphql;
 
-import info.setmy.microservice.it.RestBase;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import info.setmy.microservice.it.WebSpringBase;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
 
-import static org.hamcrest.Matchers.*;
+import java.util.Map;
+
+import static info.setmy.microservice.web.constant.ApiConstants.GRAPHQL_BASE;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-class ExampleGraphQLIT extends RestBase {
+class ExampleGraphQLIT extends WebSpringBase {
+
+    private static final String GRAPHQL_URL = "http://localhost:8777" + GRAPHQL_BASE;
+
+    private static final String QUERY = "{\"query\":\"{bookById(id: \\\"book-1\\\") {id name pageCount author {id firstName lastName}}}\",\"variables\":{},\"operationName\":null}";
 
     //https://devqa.io/rest-assured-api-requests-examples/
     @Test
     void testFindAll() {
         log.info("testFindAll");
-        getRestRequest()
-            .and()
-            .body("{\"query\":\"{bookById(id: \\\"book-1\\\") {id name pageCount author {id firstName lastName}}}\",\"variables\":{},\"operationName\":null}")
-            .post(getGraphQLUrl())
-            .then()
-            .assertThat()
-            //.contentType("application/json")
-            .statusCode(200)
-            .header("Connection", "keep-alive")
-            //.header("Content-Encoding", "gzip") ???
-            .header("Date", not(isEmptyOrNullString()))
-            .body("size()", is(1))
-            .body("data.bookById.name", equalTo("Harry Potter and the Philosopher's Stone"));
+        final ResponseEntity<String> response = RestClient.create()
+            .post()
+            .uri(GRAPHQL_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(QUERY)
+            .retrieve()
+            .toEntity(String.class);
+
+        assertThat(response.getStatusCode().value()).isEqualTo(200);
+        assertThat(response.getHeaders().getFirst("Date")).isNotBlank();
+
+        final DocumentContext json = JsonPath.parse(response.getBody());
+        final Map<String, Object> root = json.read("$");
+        assertThat(root).hasSize(1);
+        assertThat(json.<String>read("$.data.bookById.name"))
+            .isEqualTo("Harry Potter and the Philosopher's Stone");
     }
 }
